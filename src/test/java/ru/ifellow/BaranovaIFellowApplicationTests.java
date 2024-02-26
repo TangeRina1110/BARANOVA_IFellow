8b6a9dc5495699d511b268fda7bf16d82df5d434
Kate <1234567@gmail.com>
2024-02-26 10:02:07 +0300

    Initial commit

---
+++ src/test/java/ru/ifellow/BaranovaIFellowApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ifellow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaranovaIFellowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
